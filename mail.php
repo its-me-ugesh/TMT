59c8e1693d6cb0e8b7e787944398f602bcc4e55f
its-me-ugesh <61337322+its-me-ugesh@users.noreply.github.com>
2021-03-26 11:52:31 +0530

    sdw

--- mail.php
+++ mail.php
@@ -5,20 +5,20 @@
 	$name = isset( $_POST['name'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 	// $lName = isset( $_POST['lname'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['lname'] ) : "";
 	// $phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-	$email = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-	$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['contact_message'] ) : "";
+	$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+	$message = isset( $_POST['contact_message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['contact_message'] ) : "";
 	
 	//Headers
-	$to = "its.me.ugesh@gmail.com";
+	$to = "meyaqib@gmail.com";
     $subject = 'Contact Us';
-    $headers = "From: noreply@yoursite.com" . "\r\n" .
-               "CC: somebodyelse@example.com";
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 	
 	//body message
-	$mes = "First Name: ". $name . "<br> Email: ". $email . "<br> Message: " . $message . "";
+	$message = "First Name: ". $name . "<br> Last Name: ". $lName . "<br> Email: ". $senderEmail . "<br> phone: ". $phone . "<br> Message: " . $message . "";
 	
 	//Email Send Function
-    $send_email = mail($to, $subject, $mes, $headers);
+    $send_email = mail($to, $subject, $message, $headers);
       
     echo ($send_email) ? '<div class="success">Email has been sent successfully.</div>' : 'Error: Email did not send.';
 }
