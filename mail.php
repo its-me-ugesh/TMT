186821c051079a90ad6ed7dc80708bf6c8e6633b
its-me-ugesh <61337322+its-me-ugesh@users.noreply.github.com>
2021-03-26 09:04:29 +0530

    contact

--- mail.php
+++ mail.php
@@ -1,29 +1,15 @@
-<?php if(isset($_POST["name"]))  
-{
-	// Read the form values
-	$success = false;
-	$name = isset( $_POST['name'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-	// $lName = isset( $_POST['lname'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['lname'] ) : "";
-	// $phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-	$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-	$message = isset( $_POST['contact_message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['contact_message'] ) : "";
-	
-	//Headers
-	$to = "its.me.ugesh@gmail.com";
-    $subject = 'Contact Us';
-	$headers = "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	
-	//body message
-	$message = "First Name: ". $name . "<br> Last Name: ". $lName . "<br> Email: ". $senderEmail . "<br> phone: ". $phone . "<br> Message: " . $message . "";
-	
-	//Email Send Function
-    $send_email = mail($to, $subject, $message, $headers);
-      
-    echo ($send_email) ? '<div class="success">Email has been sent successfully.</div>' : 'Error: Email did not send.';
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "its.me.ugesh@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: uge.fun@gmail.com" . "\r\n";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-else
-{
-	echo '<div class="failed">Failed: Email not Sent.</div>';
-}
+//redirect
+header("Location:404.html");
 ?>
